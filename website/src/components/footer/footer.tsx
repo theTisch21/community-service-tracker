a7367703a74232e9899d9142b0fc7ec31a079d73
theTisch21 <samtisch@live.com>
2021-09-22 14:32:38 +0000

    environment variable part 1

--- website/src/components/footer/footer.tsx
+++ website/src/components/footer/footer.tsx
@@ -7,6 +7,7 @@ const Footer = () => {
       <a className="text-white" href="https://repo.ehs-service.org">
         This tracker is open source: contribute today!
       </a>
+      <p className="text-white pt-2">Authentication: {process.env.REACT_APP_AUTH}</p>
       <a className="text-white pb-2" href="mailto:eastlakekey@gmail.com">
         Questions: eastlakekey@gmail.com
       </a>
